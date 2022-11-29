5381377291f1d81a3a88e316e6e5f0765bc71239
Netesh5 <netesh.paudel@gmail.com>
2022-11-29 21:14:20 +0545

    api response

--- lib/data/response/api_response.dart
+++ lib/data/response/api_response.dart
@@ -1,11 +1,15 @@
 import 'package:mvvmapp/data/response/status.dart';
 
-class APIResponse {
+class APIResponse<T> {
   Status? status;
   String? message;
-
-  APIResponse(this.status, this.message);
+  T? data;
+  APIResponse(this.status, this.message, this.data);
   APIResponse.loading() : status = Status.Loading;
   APIResponse.completed() : status = Status.Completed;
   APIResponse.error() : status = Status.Error;
+  @override
+  String toString() {
+    return "status:$status \n message:$message \n data:$data";
+  }
 }
